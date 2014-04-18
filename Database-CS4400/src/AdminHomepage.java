4329d113b17ab898acedac8c7ae4c2fa47c022db
bochentheone@gmail.com <bochentheone@gmail.com@b600c715-1fb7-eaab-cfc8-a85c4e1f6fa8>
2014-04-18 20:25:14 +0000

    update administrator home page

--- Database-CS4400/src/AdminHomepage.java
+++ Database-CS4400/src/AdminHomepage.java
@@ -5,6 +5,10 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.Color;
 
 /**
  * The class contains the home page of administrative
@@ -20,6 +24,35 @@ public class AdminHomepage extends JPanel{
 		
 		setSize(550, 450);
 		setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Homepage");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		lblNewLabel.setBounds(221, 29, 85, 32);
+		add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Billing");
+		btnNewButton.setBackground(new Color(255, 248, 220));
+		btnNewButton.setBounds(183, 105, 140, 45);
+		add(btnNewButton);
+		
+		JButton btnDoctorPerformReport = new JButton("Doctor Perform Report");
+		btnDoctorPerformReport.setBackground(new Color(255, 245, 238));
+		btnDoctorPerformReport.setBounds(183, 171, 140, 45);
+		add(btnDoctorPerformReport);
+		
+		JButton btnSurgeryReport = new JButton("Surgery Report");
+		btnSurgeryReport.setBackground(new Color(255, 245, 238));
+		btnSurgeryReport.setBounds(183, 228, 140, 45);
+		add(btnSurgeryReport);
+		
+		JButton btnPatientVisit = new JButton("Patient Visit Report");
+		btnPatientVisit.setBackground(new Color(255, 245, 238));
+		btnPatientVisit.setBounds(183, 285, 140, 45);
+		add(btnPatientVisit);
+		
+		JButton btnNewButton_1 = new JButton("Close");
+		btnNewButton_1.setBounds(405, 386, 117, 29);
+		add(btnNewButton_1);
 		
 		
 		try {
