1e0210aaa39f0b21cce41d289ec8c77a7911f5e0
bochentheone@gmail.com <bochentheone@gmail.com@b600c715-1fb7-eaab-cfc8-a85c4e1f6fa8>
2014-04-18 18:31:34 +0000

    add go back button

--- Database-CS4400/src/PatienProfilePanel.java
+++ Database-CS4400/src/PatienProfilePanel.java
@@ -1,4 +1,6 @@
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -13,11 +15,11 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 
-/**
- * 
- */
+
 
 /**
+ * The method contains patient profile panel,
+ * it lets user input patient information
  * @author hailin
  *
  */
@@ -44,10 +46,10 @@ public class PatienProfilePanel extends JPanel{
 		
 		setSize(550, 450);
 		setLayout(null);
-		setVisible(true);
+		//setVisible(true);
 		
 		JLabel lblPatientName = new JLabel("Patient Name:");
-		lblPatientName.setBounds(87, 66, 97, 10);
+		lblPatientName.setBounds(87, 81, 97, 10);
 		add(lblPatientName);
 		
 		JLabel lblPatientProfile = new JLabel("Patient Profile");
@@ -55,95 +57,106 @@ public class PatienProfilePanel extends JPanel{
 		add(lblPatientProfile);
 		
 		JLabel lblDateOfBirth = new JLabel("Date of Birth:");
-		lblDateOfBirth.setBounds(87, 88, 85, 16);
+		lblDateOfBirth.setBounds(87, 103, 85, 16);
 		add(lblDateOfBirth);
 		
 		JLabel lblGender = new JLabel("Gender:");
-		lblGender.setBounds(87, 112, 61, 16);
+		lblGender.setBounds(87, 138, 61, 16);
 		add(lblGender);
 		
 		JLabel lblAddress = new JLabel("Address:");
-		lblAddress.setBounds(87, 140, 61, 16);
+		lblAddress.setBounds(87, 168, 61, 16);
 		add(lblAddress);
 		
 		JLabel lblHomePhone = new JLabel("Home Phone:");
-		lblHomePhone.setBounds(87, 168, 97, 16);
+		lblHomePhone.setBounds(87, 200, 97, 16);
 		add(lblHomePhone);
 		
 		JLabel lblWorkPhone = new JLabel("Work Phone:");
-		lblWorkPhone.setBounds(87, 196, 85, 16);
+		lblWorkPhone.setBounds(87, 237, 85, 16);
 		add(lblWorkPhone);
 		
 		JLabel lblWeight = new JLabel("Weight:");
-		lblWeight.setBounds(87, 224, 61, 16);
+		lblWeight.setBounds(87, 265, 61, 16);
 		add(lblWeight);
 		
 		JLabel lblHeight = new JLabel("Height:");
-		lblHeight.setBounds(87, 252, 61, 16);
+		lblHeight.setBounds(87, 295, 61, 16);
 		add(lblHeight);
 		
 		JLabel lblAnnualIncome = new JLabel("Annual Income:");
-		lblAnnualIncome.setBounds(87, 280, 112, 16);
+		lblAnnualIncome.setBounds(87, 328, 112, 16);
 		add(lblAnnualIncome);
 		
 		JLabel lblAllergies = new JLabel("Allergies:");
-		lblAllergies.setBounds(87, 308, 85, 16);
+		lblAllergies.setBounds(87, 360, 85, 16);
 		add(lblAllergies);
 		
 		nameField = new JTextField();
-		nameField.setBounds(241, 57, 134, 28);
+		nameField.setBounds(241, 72, 134, 28);
 		add(nameField);
 		nameField.setColumns(10);
 		
 		birthdateField = new JTextField();
 		birthdateField.setToolTipText("format:1990-01-01");
 		birthdateField.setColumns(10);
-		birthdateField.setBounds(241, 82, 134, 28);
+		birthdateField.setBounds(241, 103, 134, 28);
 		add(birthdateField);
 		
 		addressField = new JTextField();
 		addressField.setColumns(10);
-		addressField.setBounds(241, 134, 134, 28);
+		addressField.setBounds(241, 162, 134, 28);
 		add(addressField);
 		
 		homePhoneField = new JTextField();
 		homePhoneField.setColumns(10);
-		homePhoneField.setBounds(241, 162, 134, 28);
+		homePhoneField.setBounds(241, 194, 134, 28);
 		add(homePhoneField);
 		
 		workPhoneField = new JTextField();
 		workPhoneField.setColumns(10);
-		workPhoneField.setBounds(241, 190, 134, 28);
+		workPhoneField.setBounds(241, 228, 134, 28);
 		add(workPhoneField);
 		
 		weightField = new JTextField();
 		weightField.setColumns(10);
-		weightField.setBounds(241, 218, 134, 28);
+		weightField.setBounds(241, 259, 134, 28);
 		add(weightField);
 		
 		heightField = new JTextField();
 		heightField.setColumns(10);
-		heightField.setBounds(241, 246, 134, 28);
+		heightField.setBounds(241, 289, 134, 28);
 		add(heightField);
 		
 		incomeField = new JTextField();
 		incomeField.setColumns(10);
-		incomeField.setBounds(241, 274, 134, 28);
+		incomeField.setBounds(241, 322, 134, 28);
 		add(incomeField);
 		
 		allergiesField = new JTextField();
 		allergiesField.setColumns(10);
-		allergiesField.setBounds(241, 302, 134, 28);
+		allergiesField.setBounds(241, 354, 134, 28);
 		add(allergiesField);
 		
 		JButton btnSubmit = new JButton("Submit");
-		btnSubmit.setBounds(357, 342, 117, 29);
+		btnSubmit.setBounds(383, 393, 117, 29);
 		add(btnSubmit);
 		
 		String[] gender = {"Male", "Female"};
 		JComboBox genderComboBox = new JComboBox(gender);
-		genderComboBox.setBounds(241, 108, 134, 27);
+		genderComboBox.setBounds(241, 134, 134, 27);
 		add(genderComboBox);
+		
+		JButton btnGoBack = new JButton("GO BACK");
+		btnGoBack.setBounds(31, 393, 117, 29);
+		add(btnGoBack);
+		btnGoBack.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				removeAll();
+				add(new LoginPanel());
+				
+			}
+		});
 
 	}
 
