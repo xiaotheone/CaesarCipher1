ad03956fa555523385c7fa699e1af64aa182f690
guochongwu@gmail.com <guochongwu@gmail.com@b600c715-1fb7-eaab-cfc8-a85c4e1f6fa8>
2014-04-20 20:07:02 +0000

    patientmessage panel finished

--- Database-CS4400/src/PatientMessagePanel.java
+++ Database-CS4400/src/PatientMessagePanel.java
@@ -2,14 +2,21 @@ import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.border.LineBorder;
+
 import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JMenu;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import com.jgoodies.forms.factories.DefaultComponentFactory;
+
 import javax.swing.JTextArea;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
@@ -22,6 +29,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.JButton;
+
 /**
  * 
  */
@@ -31,6 +40,9 @@ import java.util.List;
  *
  */
 public class PatientMessagePanel extends JPanel{
+	private JLabel s1 =new JLabel();
+	private JLabel s2 =new JLabel();
+	private JLabel s3=new JLabel();
 	
 	public static BufferedImage image;
 	private static Connection conn = ConnectionManager.getInstance().getConnection();
@@ -40,7 +52,7 @@ public class PatientMessagePanel extends JPanel{
 
 		setSize(550, 450);
 		setLayout(null);
-		String[] status = getStatus();
+		
 		try {
 			image = ImageIO.read(new File("Images/buzz.png"));
 		} catch (IOException e) {
@@ -58,74 +70,18 @@ public class PatientMessagePanel extends JPanel{
 		add(lblStatus);
 		
 		JLabel lblDate = new JLabel("Date");
-		lblDate.setBounds(86, 102, 48, 16);
+		lblDate.setBounds(108, 102, 48, 16);
 		add(lblDate);
 		
 		JLabel lblFrom = new JLabel("From");
-		lblFrom.setBounds(165, 102, 61, 16);
+		lblFrom.setBounds(221, 102, 61, 16);
 		add(lblFrom);
 		
 		JLabel lblMessage = new JLabel("Message");
-		lblMessage.setBounds(244, 102, 61, 16);
+		lblMessage.setBounds(333, 102, 61, 16);
 		add(lblMessage);
 		
-		if(status.length > 0){
-			JLabel lblStatus_1_1 = new JLabel();
-			lblStatus_1_1.setBounds(17, 143, 47, 16);
-			add(lblStatus_1_1);
-			
-			JLabel lblDate_1 = new JLabel("Date1");
-			lblDate_1.setBounds(86, 143, 61, 16);
-			add(lblDate_1);
-			
-			JLabel lblSender = new JLabel("Sender1");
-			lblSender.setBounds(165, 143, 61, 16);
-			add(lblSender);
-			
-			JLabel lblMessage_1 = new JLabel("Message1");
-			lblMessage_1.setBounds(244, 143, 200, 16);
-			add(lblMessage_1);
-		}
 		
-		if(status.length > 1) {
-			JLabel lblStatus_1 = new JLabel();
-			lblStatus_1.setBounds(17, 186, 61, 16);
-			add(lblStatus_1);
-			
-			JLabel lblDate_2 = new JLabel("Date2");
-			lblDate_2.setBounds(86, 186, 61, 16);
-			add(lblDate_2);
-			
-			JLabel lblSender_1 = new JLabel("Sender2");
-			lblSender_1.setBounds(165, 186, 61, 16);
-			add(lblSender_1);
-	
-			
-			JLabel lblMessage_2 = new JLabel("Message2");
-			lblMessage_2.setBounds(244, 186, 200, 16);
-			add(lblMessage_2);
-		}
-		
-		
-		if(status.length > 2) {
-			JLabel lblStatus_2 = new JLabel();
-			lblStatus_2.setBounds(17, 231, 61, 16);
-			add(lblStatus_2);
-	
-			JLabel lblDate_3 = new JLabel("Date3");
-			lblDate_3.setBounds(86, 231, 61, 16);
-			add(lblDate_3);
-			
-	
-			JLabel lblSender_2 = new JLabel("Sender3");
-			lblSender_2.setBounds(165, 231, 61, 16);
-			add(lblSender_2);
-			
-			
-			JLabel lblMessage_3 = new JLabel("Message3");
-			lblMessage_3.setBounds(244, 231, 200, 16);
-			add(lblMessage_3);
-		}
 		
 		JLabel lblBack = new JLabel("Back");
 		lblBack.addMouseListener(new MouseAdapter() {
@@ -143,6 +99,97 @@ public class PatientMessagePanel extends JPanel{
 		});
 		lblBack.setBounds(383, 259, 61, 16);
 		add(lblBack);
+		
+		
+		String[] status = getStatus();
+		if(status.length > 0){
+			
+			s1.setBounds(17, 143, 47, 16);
+			add(s1);
+			s1.setText(this.getStatus()[0]);
+			
+			JLabel d1 = new JLabel("Date1");
+			d1.setBounds(88, 143, 113, 16);
+			add(d1);
+			d1.setText(this.getTime()[0]);
+			
+			JLabel f1 = new JLabel("Sender1");
+			f1.setBounds(221, 143, 61, 16);
+			add(f1);
+			f1.setText(this.getSender()[0]);
+			
+			
+			JLabel m1 = new JLabel("Message1");
+			m1.setBounds(300, 143, 200, 16);
+			add(m1);
+			m1.setText(this.getContent()[0]);
+		}
+		
+		if(status.length > 1) {
+			
+			s2.setBounds(17, 186, 61, 16);
+			add(s2);
+			s2.setText(this.getStatus()[1]);
+			
+			
+			JLabel d2 = new JLabel("Date2");
+			d2.setBounds(88, 186, 113, 16);
+			add(d2);
+			d2.setText(this.getTime()[1]);
+			
+			
+			JLabel f2 = new JLabel("Sender2");
+			f2.setBounds(221, 186, 61, 16);
+			add(f2);
+			f2.setText(this.getSender()[1]);
+			
+			JLabel m2 = new JLabel("Message2");
+			m2.setBounds(300, 186, 200, 16);
+			add(m2);
+			m2.setText(this.getContent()[1]);
+		}
+		
+		
+		if(status.length > 2) {
+		
+			s3.setBounds(17, 231, 61, 16);
+			add(s3);
+			s3.setText(this.getStatus()[2]);
+	
+			JLabel d3 = new JLabel("Date3");
+			d3.setBounds(88, 231, 113, 16);
+			add(d3);
+			d3.setText(this.getTime()[2]);
+			
+	
+			JLabel f3 = new JLabel("Sender3");
+			f3.setBounds(221, 231, 61, 16);
+			add(f3);
+			f3.setText(this.getSender()[2]);
+			
+			JLabel m3 = new JLabel("Message3");
+			m3.setBounds(300, 231, 200, 16);
+			add(m3);
+			m3.setText(this.getContent()[2]);
+		}
+		JButton btnMarkAsRead = new JButton("Mark As Read");
+		btnMarkAsRead.setBounds(333, 64, 133, 31);
+		add(btnMarkAsRead);
+		btnMarkAsRead.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				try {
+					markAsRead();
+					display();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+			
+		});
 	}
 	
 	public void paintComponent(Graphics g) {
@@ -158,13 +205,19 @@ public class PatientMessagePanel extends JPanel{
 		List<String> statusList = new ArrayList<String>();
 		try(PreparedStatement stmt = conn.prepareStatement(SQL);){
 			stmt.setString(1, currentPatient.cp.getPatientUsername());
-			stmt.setInt(2, 0);
+			stmt.setString(2, "0");
 			stmt.setString(3, currentPatient.cp.getPatientUsername());
-			stmt.setInt(4, 1);
+			stmt.setString(4, "1");
 			
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next()){
-				statusList.add(rs.getString("Status"));
+				String s = rs.getString("Status");
+				if(s.equals("0")){
+					statusList.add("Unread");
+				}else if (s.equals("1")){
+					statusList.add("Read");
+				}
+				;
 			}
 		} catch (SQLException e){
 			System.out.println("Error");
@@ -176,12 +229,110 @@ public class PatientMessagePanel extends JPanel{
 		}
 		return status;
 	}
-	
-	public String[] getContent(){
-		return null;
+
+	public String[] getContent() {
+		String SQL = "SELECT Content FROM SendsMessageToPatient WHERE PatUsername = ?";
+
+		List<String> contentList = new ArrayList<String>();
+		try (PreparedStatement stmt = conn.prepareStatement(SQL);) {
+			stmt.setString(1, currentPatient.cp.getPatientUsername());
+
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+
+				contentList.add(rs.getString("Content"));
+			}
+		} catch (SQLException e) {
+			System.out.println("Error");
+		}
+
+		String[] content = (String[]) contentList
+				.toArray(new String[contentList.size()]);
+		for (int i = 0; i < content.length; i++) {
+			System.out.println(content[i]);
+		}
+		return content;
 	}
-	
-	public String[] getTime(){
-		return null;
+
+	public String[] getTime() {
+		String SQL = "SELECT DateTime FROM SendsMessageToPatient WHERE PatUsername = ?";
+
+		List<String> timeList = new ArrayList<String>();
+		try (PreparedStatement stmt = conn.prepareStatement(SQL);) {
+			stmt.setString(1, currentPatient.cp.getPatientUsername());
+
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+
+				timeList.add(rs.getString("DateTime").substring(0, 10));
+			}
+		} catch (SQLException e) {
+			System.out.println("Error");
+		}
+
+		String[] time = (String[]) timeList
+				.toArray(new String[timeList.size()]);
+		for (int i = 0; i < time.length; i++) {
+			System.out.println(time[i]);
+		}
+		return time;
 	}
+
+	public String[] getSender() {
+		String SQL = "SELECT Lname FROM Doctor AS D, SendsMessageToPatient AS S WHERE S.DocUsername = D.DocUsername AND PatUsername = ?";
+
+		List<String> senderList = new ArrayList<String>();
+		try (PreparedStatement stmt = conn.prepareStatement(SQL);) {
+			stmt.setString(1, currentPatient.cp.getPatientUsername());
+
+			ResultSet rs = stmt.executeQuery();
+			while (rs.next()) {
+
+				senderList.add("Dr. " + rs.getString("Lname"));
+			}
+		} catch (SQLException e) {
+			System.out.println("Error");
+		}
+
+		String[] sender = (String[]) senderList.toArray(new String[senderList
+				.size()]);
+		for (int i = 0; i < sender.length; i++) {
+			System.out.println(sender[i]);
+		}
+		return sender;
+	}
+		public boolean markAsRead() throws SQLException{
+			String SQL = "UPDATE SendsMessageToPatient SET	Status = \"1\"  WHERE	PatUsername =?";
+		try (PreparedStatement stmt = conn.prepareStatement(SQL);) {	
+			stmt.setString(1, currentPatient.cp.getPatientUsername());
+			
+			
+			int affected = stmt.executeUpdate();
+			if (affected == 1) {
+				System.out.println("marked");
+				JOptionPane.showMessageDialog(getParent(), "marked");
+
+				return true;
+			} else {
+				System.err.println("error");
+				JOptionPane.showMessageDialog(getParent(), "error");
+				return false;
+			}
+		}
+	}
+		public void display(){
+			String[] status = getStatus();
+			if(status.length > 0){
+				s1.setText((this.getStatus()[0]));
+			}
+			
+			if(status.length > 1) {
+				s2.setText((this.getStatus()[1]));
+			}
+			
+			
+			if(status.length > 2) {
+				s3.setText((this.getStatus()[2]));
+			}
+		}
 }
