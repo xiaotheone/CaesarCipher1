bafd0c0d1b73bcf6e22e89001f57f366d698ab86
bochentheone@gmail.com <bochentheone@gmail.com@b600c715-1fb7-eaab-cfc8-a85c4e1f6fa8>
2014-04-22 02:01:38 +0000

    update record surgery panel

--- Database-CS4400/src/RecordSurgeryPanel.java
+++ Database-CS4400/src/RecordSurgeryPanel.java
@@ -1,13 +1,23 @@
 import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JTable;
@@ -18,14 +28,14 @@ import javax.swing.JComboBox;
 public class RecordSurgeryPanel extends JPanel{
 	private static Connection conn = ConnectionManager.getInstance().getConnection();
 	public static BufferedImage image;
-	private JTextField textField;
-	private JTable patientTable;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JTextField inputPatientField;
+	private JTextField nametextField;
+	private JTextField surgerytextField;
+	private JTextField CPTField;
+	private JTextField anesthesiaField;
+	private JTextField surgeryStartField;
+	private JTextField surgeryCompleteField;
+	private JComboBox comboPatient;
 	
 	public RecordSurgeryPanel(){
 		try {
@@ -45,18 +55,29 @@ public class RecordSurgeryPanel extends JPanel{
 		lblNewLabel.setBounds(69, 75, 92, 16);
 		add(lblNewLabel);
 		
-		textField = new JTextField();
-		textField.setBounds(170, 69, 134, 28);
-		add(textField);
-		textField.setColumns(10);
+		
+		inputPatientField = new JTextField();
+		inputPatientField.setBounds(170, 69, 134, 28);
+		add(inputPatientField);
+		inputPatientField.setColumns(10);
 		
 		JButton btnSearch = new JButton("Search");
 		btnSearch.setBounds(314, 70, 117, 29);
 		add(btnSearch);
-		
-		patientTable = new JTable();
-		patientTable.setBounds(10, 102, 294, 50);
-		add(patientTable);
+		btnSearch.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				try {
+					System.out.println("searching patient");
+					searchPatient();
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					System.err.println(e1);
+				}
+			}
+		});
 		
 		JLabel lblNewLabel_1 = new JLabel("Patient Name");
 		lblNewLabel_1.setBounds(10, 195, 83, 16);
@@ -82,73 +103,116 @@ public class RecordSurgeryPanel extends JPanel{
 		lblPreoperative.setBounds(6, 347, 96, 16);
 		add(lblPreoperative);
 		
-		JTextArea textArea = new JTextArea();
-		textArea.setBounds(124, 347, 92, 38);
-		add(textArea);
+		JTextArea operativeArea = new JTextArea();
+		operativeArea.setBounds(124, 347, 104, 38);
+		add(operativeArea);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(114, 189, 114, 28);
-		add(textField_1);
-		textField_1.setColumns(10);
+		nametextField = new JTextField();
+		nametextField.setBounds(114, 189, 114, 28);
+		add(nametextField);
+		nametextField.setColumns(10);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(114, 217, 114, 28);
-		add(textField_2);
+		surgerytextField = new JTextField();
+		surgerytextField.setColumns(10);
+		surgerytextField.setBounds(114, 217, 114, 28);
+		add(surgerytextField);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(114, 273, 114, 28);
-		add(textField_3);
+		CPTField = new JTextField();
+		CPTField.setColumns(10);
+		CPTField.setBounds(114, 273, 114, 28);
+		add(CPTField);
 		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(114, 247, 114, 27);
-		add(comboBox);
+		JComboBox comboProcedure = new JComboBox();
+		comboProcedure.setBounds(114, 247, 114, 27);
+		add(comboProcedure);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(114, 303, 114, 27);
-		add(comboBox_1);
+		JComboBox comboNoAss = new JComboBox();
+		comboNoAss.setBounds(114, 303, 114, 27);
+		add(comboNoAss);
 		
 		JLabel lblNewLabel_3 = new JLabel("Anesthesia Start Time");
 		lblNewLabel_3.setBounds(256, 195, 122, 16);
 		add(lblNewLabel_3);
 		
-		textField_4 = new JTextField();
-		textField_4.setBounds(390, 189, 83, 28);
-		add(textField_4);
-		textField_4.setColumns(10);
+		anesthesiaField = new JTextField();
+		anesthesiaField.setBounds(390, 189, 83, 28);
+		add(anesthesiaField);
+		anesthesiaField.setColumns(10);
 		
 		JLabel lblSurgeryStartTime = new JLabel("Surgery Start time");
 		lblSurgeryStartTime.setBounds(256, 223, 122, 16);
 		add(lblSurgeryStartTime);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(390, 217, 83, 28);
-		add(textField_5);
+		surgeryStartField = new JTextField();
+		surgeryStartField.setColumns(10);
+		surgeryStartField.setBounds(390, 217, 83, 28);
+		add(surgeryStartField);
 		
 		JLabel lblSurgeryCompleteTime = new JLabel("Surgery Complete time");
 		lblSurgeryCompleteTime.setBounds(256, 251, 122, 16);
 		add(lblSurgeryCompleteTime);
 		
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		textField_6.setBounds(390, 245, 83, 28);
-		add(textField_6);
+		surgeryCompleteField = new JTextField();
+		surgeryCompleteField.setColumns(10);
+		surgeryCompleteField.setBounds(390, 245, 83, 28);
+		add(surgeryCompleteField);
 		
 		JLabel lblDescription = new JLabel("Description:");
-		lblDescription.setBounds(256, 307, 61, 16);
+		lblDescription.setBounds(256, 307, 109, 16);
 		add(lblDescription);
 		
-		JTextArea textArea_1 = new JTextArea();
-		textArea_1.setBounds(337, 303, 136, 82);
-		add(textArea_1);
+		JTextArea descriptionArea = new JTextArea();
+		descriptionArea.setBounds(377, 307, 136, 82);
+		add(descriptionArea);
 		
 		JButton btnRecord = new JButton("Record");
 		btnRecord.setBounds(204, 398, 117, 29);
 		add(btnRecord);
 		
+		JLabel lblPatientName = new JLabel("Patient Name      Phone Number");
+		lblPatientName.setBounds(10, 103, 200, 21);
+		add(lblPatientName);
 		
+		JButton btnSelect = new JButton("Select");
+		btnSelect.setBounds(228, 125, 117, 29);
+		add(btnSelect);
+		
+		comboPatient = new JComboBox();
+		comboPatient.setBounds(10, 126, 218, 27);
+		add(comboPatient);
+		
+		
+	}
+	
+	public void searchPatient() throws SQLException{
+		
+		String SQL = "SELECT Name, HomePhone FROM Patient WHERE	Name = ?";
+		List<String> list = new ArrayList<String>();
+		String patientNamePhone = "";
+		
+		ResultSet rs = null;
+		
+		try (PreparedStatement stmt = conn.prepareStatement(SQL)){
+			
+			stmt.setString(1, this.inputPatientField.getText());
+			
+			rs = stmt.executeQuery();
+
+			while(rs.next()){
+
+				list.add(rs.getString("Name") + "     " + rs.getString("HomePhone"));
+				
+				System.out.println(rs.getString("Name") + rs.getString("HomePhone"));
+			}
+			String[] temp = new String[list.size()];
+			list.toArray(temp);
+			comboPatient.setModel(new DefaultComboBoxModel(temp));
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.err.println(e);
+		}
+
 	}
 	
 	public void paintComponent(Graphics g) {
