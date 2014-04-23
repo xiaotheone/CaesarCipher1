65dfe229f4fed6f74451370b1ef006b7db72ac9b
guochongwu@gmail.com <guochongwu@gmail.com@b600c715-1fb7-eaab-cfc8-a85c4e1f6fa8>
2014-04-23 01:52:40 +0000

    patient visit report finished

--- Database-CS4400/src/AdminPatientVisitReportPanel.java
+++ Database-CS4400/src/AdminPatientVisitReportPanel.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -12,6 +13,10 @@ import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
 import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.TableColumn;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
@@ -31,9 +36,16 @@ public class AdminPatientVisitReportPanel extends JPanel{
 	private static Connection conn = ConnectionManager.getInstance().getConnection();
 	JComboBox CbMonth;
 	JComboBox CbYear;
+	
+	private Object[][] data;
+	private int rows=0;
+	private JTable table = new JTable();
+	private JScrollPane scrollPane = new JScrollPane();
+	private String[] columnNames = new String[4];
 	public AdminPatientVisitReportPanel() throws SQLException{
 		setSize(550, 450);
 		setLayout(null);
+	
 		
 		JLabel lblPatientVisitReport = new JLabel("Patient Visit Report ");
 		lblPatientVisitReport.setBounds(196, 28, 153, 16);
@@ -57,7 +69,13 @@ public class AdminPatientVisitReportPanel extends JPanel{
 		btnGenerateReport.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
+				
+					remove(scrollPane);
+					
 					patientVisitReport();
+					revalidate();
+					repaint();
+	
 				} catch (SQLException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -91,12 +109,8 @@ public class AdminPatientVisitReportPanel extends JPanel{
 		repaint();
 	}
 	
-	public void patientVisitReport() throws SQLException{
-		
-		
-		// get numbers of patient each doctor has seem
-		String Fname, Lname = "";
-		int patientNum = 0;
+	public int getrows() throws SQLException {
+
 		String SQL = "SELECT Fname, Lname, Count(*) AS PatientSeem "
 					+ "FROM Doctor JOIN Visit "
 					+ "ON Doctor.DocUsername = Visit.DocUsername "
@@ -107,12 +121,43 @@ public class AdminPatientVisitReportPanel extends JPanel{
 			stmt.setString(1, CbMonth.getSelectedItem().toString());
 			stmt.setString(2, CbYear.getSelectedItem().toString());
 			ResultSet rs = stmt.executeQuery();
+			
+			rs.last();
+			return rs.getRow();
+		
+		}
+	}
+	public void patientVisitReport() throws SQLException{
+		addTable();
+		
+		// get numbers of patient each doctor has seem
+		String Fname, Lname = "";
+		int patientNum = 0;
+		String SQL = "SELECT Fname, Lname, Count(*) AS PatientSeem "
+					+ "FROM Doctor JOIN Visit "
+					+ "ON Doctor.DocUsername = Visit.DocUsername "
+					+ "WHERE MONTH(DateofVisit) = ? AND YEAR(DateofVisit) = ? "
+					+ "GROUP BY Doctor.DocUsername";
+		Object[][] s = new Object[getrows()][2]; 
+		try(PreparedStatement stmt = conn.prepareStatement(SQL);){
+			stmt.setString(1, CbMonth.getSelectedItem().toString());
+			stmt.setString(2, CbYear.getSelectedItem().toString());
+			ResultSet rs = stmt.executeQuery();
 			while(rs.next()){
 				Fname = rs.getString("Fname");
 				Lname = rs.getString("Lname");
 				patientNum = rs.getInt("PatientSeem");
+				s[rs.getRow()-1][0]= new String(Fname +" "+ Lname);
+				
+				s[rs.getRow()-1][1] = new Integer(patientNum);
 				
 				System.out.println("Doctor Name: " + Fname + " " + Lname + "   Patient Seem: " + patientNum );
+			}
+			for(int x =0;x<s.length;x++){
+				for(int y =0;y<s[x].length;y++){
+					System.out.println(s[x][y]);
+					data[x][y]= s[x][y];
+				}
 			}
 		}
 		
@@ -124,6 +169,7 @@ public class AdminPatientVisitReportPanel extends JPanel{
 					   + "WHERE MONTH(DateofVisit) = ? AND YEAR(DateofVisit) = ? "
 					   + "GROUP BY Doctor.DocUsername";
 					  
+		Object[] s2 = new Object[getrows()]; 
 		try(PreparedStatement stmt = conn.prepareStatement(SQL1);){
 			stmt.setString(1, CbMonth.getSelectedItem().toString());
 			stmt.setString(2, CbYear.getSelectedItem().toString());
@@ -132,9 +178,18 @@ public class AdminPatientVisitReportPanel extends JPanel{
 				Fname = rs.getString("Fname");
 				Lname = rs.getString("Lname");
 				prescriptionNum = rs.getInt("PrescriptionNum");
+			
 				System.out.println("Doctor Name: " + Fname + " " + Lname + "   Number of Prescription: " + prescriptionNum );
+				s2[rs.getRow()-1]= new Integer(prescriptionNum);
+				
+				for(int x =0;x<s2.length;x++){
+
+						data[x][2]= s2[x];
+					}
+				}
 			}
-		}
+		
+		
 		
 		
 		//get visit bill
@@ -143,7 +198,8 @@ public class AdminPatientVisitReportPanel extends JPanel{
 						+ "FROM Doctor JOIN Visit ON Doctor.DocUsername = Visit.DocUsername "
 						+ "WHERE MONTH(DateofVisit) = ? AND YEAR(DateofVisit) = ? "
 						+ "GROUP BY Doctor.DocUsername";
-		
+		int total;
+		int[] s3 = new int[getrows()];
 		try(PreparedStatement stmt = conn.prepareStatement(SQL2);){
 			stmt.setString(1, CbMonth.getSelectedItem().toString());
 			stmt.setString(2, CbYear.getSelectedItem().toString());
@@ -153,6 +209,9 @@ public class AdminPatientVisitReportPanel extends JPanel{
 				Lname = rs.getString("Lname");
 				visitBill = rs.getInt("TotalBill");
 				System.out.println("Doctor Name: " + Fname + " " + Lname + "   total Visit Bill: " + visitBill );
+		
+				s3[rs.getRow()-1]= visitBill;
+				
 			}
 		}
 		
@@ -174,7 +233,51 @@ public class AdminPatientVisitReportPanel extends JPanel{
 				Lname = rs.getString("Lname");
 				surgeryBill = rs.getInt("TotalSurgeryBill");
 				System.out.println("Doctor Name: " + Fname + " " + Lname + "   total Surgery Bill: " + surgeryBill );
+				s3[rs.getRow()-1] += surgeryBill;
 			}
+			
+			
+			for(int x =0;x<s3.length;x++){
+
+					data[x][3]= s3[x];
+				}
 		}
+		
+	}
+	public void addTable() throws SQLException{
+
+	    columnNames[0]= "Doctor Name";
+	    columnNames[1]= "No of patient seen";
+	    columnNames[2]= "prescription written";
+	    columnNames[3]= "Total billing $";
+	    rows =getrows();
+		data = new Object[rows][4];
+		for ( int i =0; i<data.length;i++){
+			for ( int j =0; j<data[i].length;j++)
+				data[i][j] = new String ("0");
+		}
+		table = new JTable(data, columnNames);
+		table.setBounds(37, 79, 449, 344);
+
+		table.setFillsViewportHeight(true);
+
+		table.getTableHeader();
+	
+		table.setRowHeight(30);
+		
+		   scrollPane = new JScrollPane(table);
+		  scrollPane.setPreferredSize(new Dimension(500,table.getHeight()));
+		  scrollPane.setBounds(37, 129, 449, 264);
+		    TableColumn column = null;
+		    for (int i = 0; i < 3; i++) {
+		        column = table.getColumnModel().getColumn(i);
+		        if (i == 2) {
+		            column.setPreferredWidth(100);
+		        } else {
+		            column.setPreferredWidth(100);
+		        }
+		    }  
+
+		this.add(scrollPane);
 	}
 }
